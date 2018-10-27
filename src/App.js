2b2319a2a9b9b29f083d726066ea6d0ed79dc4bd
Владимир Дельгадильо <vladimirdelgadilo@MacBook-Pro-Vladimir.local>
2018-10-27 12:32:16 +0300

    Filter also applies to the playlist counter and hours counter

--- src/App.js
+++ src/App.js
@@ -113,6 +113,12 @@ class App extends Component {
   }
 
   render() {
+    let playlistsToRender = this.state.serverData.user ? 
+    this.state.serverData.user.playlists
+      .filter( playlist =>
+        playlist.name.toLowerCase().includes(
+          this.state.filterString.toLowerCase())
+      ) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -120,14 +126,10 @@ class App extends Component {
           <h1>
           {this.state.serverData.user.name}'s Playlists
           </h1>
-          <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
-          <HoursCounter playlists={this.state.serverData.user.playlists}/>
+          <PlaylistCounter playlists={playlistsToRender}/>
+          <HoursCounter playlists={playlistsToRender}/>
           <Filter onTextChange={text => this.setState({filterString: text})}/>
-          {
-            this.state.serverData.user.playlists.filter( playlist =>
-              playlist.name.toLowerCase().includes(
-                this.state.filterString.toLowerCase())
-            ).map(playlist => 
+          {playlistsToRender.map(playlist => 
               <Playlist playlist={playlist}/>
             )}
         </div> : <h1>Loading...</h1>
